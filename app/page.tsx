46b837e140a57f88d533327f0983ea22d3917811
Piotr Maciejewski <pether.maciejewski@gmail.com>
2023-08-13 18:05:21 +0200

    home.ts update

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
-import { fetchAllProjects } from "@/lib/actions";
 import { ProjectInterface } from "@/common.types";
-import ProjectCard from "@/components/ProjectCard";
 import Categories from "@/components/Categories";
 import LoadMore from "@/components/LoadMore";
+import ProjectCard from "@/components/ProjectCard";
+import { fetchAllProjects } from "@/lib/actions";
+
+type SearchParams = {
+  category?: string | null;
+  endcursor?: string | null;
+}
+
+type Props = {
+  searchParams: SearchParams
+}
 
 type ProjectSearch = {
   projectSearch: {
@@ -13,23 +22,15 @@ type ProjectSearch = {
       startCursor: string;
       endCursor: string;
     };
-  };
-};
+  },
+}
 
-type SearchParams = {
-  category?: string;
-  endcursor?: string;
-};
-type Props = {
-  searchParams: SearchParams;
-};
-
-export const dynamic = "force-dynamic";
+export const dynamic = 'force-dynamic';
 export const dynamicParams = true;
 export const revalidate = 0;
 
 const Home = async ({ searchParams: { category, endcursor } }: Props) => {
-  const data = await fetchAllProjects(category, endcursor) as ProjectSearch;
+  const data = await fetchAllProjects(category, endcursor) as ProjectSearch
 
   const projectsToDisplay = data?.projectSearch?.edges || [];
 
@@ -37,40 +38,38 @@ const Home = async ({ searchParams: { category, endcursor } }: Props) => {
     return (
       <section className="flexStart flex-col paddings">
         <Categories />
-        <p className="no-result-text text-center">
-          No projects found, go create some first.
-        </p>
+
+        <p className="no-result-text text-center">No projects found, go create some first.</p>
       </section>
-    );
+    )
   }
 
-  const pagination = data?.projectSearch?.pageInfo;
-
   return (
-    <section className="flex-start flex-cols paddings mb-16">
+    <section className="flexStart flex-col paddings mb-16">
       <Categories />
+
       <section className="projects-grid">
         {projectsToDisplay.map(({ node }: { node: ProjectInterface }) => (
           <ProjectCard
-            key={node?.id}
+            key={`${node?.id}`}
             id={node?.id}
             image={node?.image}
             title={node?.title}
-            name={node?.createdBy?.name}
-            avatarUrl={node?.createdBy?.avatarUrl}
-            userId={node?.createdBy?.id}
+            name={node?.createdBy.name}
+            avatarUrl={node?.createdBy.avatarUrl}
+            userId={node?.createdBy.id}
           />
         ))}
       </section>
 
-      <LoadMore
-        startCursor={pagination.startCursor}
-        endCursor={pagination.endCursor}
-        hasNextPage={pagination.hasNextPage}
-        hasPreviousPage={pagination.hasPreviousPage}
+      <LoadMore 
+        startCursor={data?.projectSearch?.pageInfo?.startCursor} 
+        endCursor={data?.projectSearch?.pageInfo?.endCursor} 
+        hasPreviousPage={data?.projectSearch?.pageInfo?.hasPreviousPage} 
+        hasNextPage={data?.projectSearch?.pageInfo.hasNextPage}
       />
     </section>
-  );
+  )
 };
 
 export default Home;
