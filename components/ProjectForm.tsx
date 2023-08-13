62f805d212a62b1ef3344a4920b9b66e9b49ec53
Piotr Maciejewski <pether.maciejewski@gmail.com>
2023-08-13 21:13:42 +0200

    projectForm update

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -1,14 +1,15 @@
 "use client";
 
-import { ChangeEvent, useState } from "react";
-import { ProjectInterface, SessionInterface } from "@/common.types";
-import FormField from "@/components/FormField";
-import { categoryFilters } from "@/constants";
-import CustomMenu from "./CustomMenu";
-import Button from "./Button";
 import Image from "next/image";
-import { createNewProject, fetchToken, updateProject } from "@/lib/actions";
+import React, { ChangeEvent, FormEvent, useState } from "react";
 import { useRouter } from "next/navigation";
+
+import FormField from "./FormField";
+import Button from "./Button";
+import CustomMenu from "./CustomMenu";
+import { categoryFilters } from "@/constants";
+import { updateProject, createNewProject, fetchToken } from "@/lib/actions";
+import { FormState, ProjectInterface, SessionInterface } from "@/common.types";
 
 type Props = {
   type: string;
@@ -19,60 +20,8 @@ type Props = {
 const ProjectForm = ({ type, session, project }: Props) => {
   const router = useRouter();
 
-  const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    setIsSubmitting(true);
-
-    const { token } = await fetchToken();
-
-    try {
-      if (type === "create") {
-        // create project
-        await createNewProject(form, session?.user?.id, token);
-
-        router.push("/");
-      }
-
-      if (type === "edit") {
-        // edit project
-        await updateProject(form, project?.id as string, token);
-
-        router.push("/");
-      }
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setIsSubmitting(false);
-    }
-  };
-
-  const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault();
-    const file = e.target.files?.[0];
-    if (!file) return;
-
-    if (!file.type.includes("image")) {
-      return alert("Please upload an image file");
-    }
-
-    const reader = new FileReader();
-
-    reader.readAsDataURL(file);
-    reader.onload = () => {
-      const result = reader.result as string;
-
-      handleStateChange("image", result);
-    };
-  };
-
-  const handleStateChange = (fieldName: string, value: string) => {
-    setForm((prevState) => ({ ...prevState, [fieldName]: value }));
-  };
-
-  const [isSubmitting, setIsSubmitting] = useState(false);
-
-  const [form, setForm] = useState({
+  const [submitting, setSubmitting] = useState<boolean>(false);
+  const [form, setForm] = useState<FormState>({
     title: project?.title || "",
     description: project?.description || "",
     image: project?.image || "",
@@ -80,6 +29,64 @@ const ProjectForm = ({ type, session, project }: Props) => {
     githubUrl: project?.githubUrl || "",
     category: project?.category || "",
   });
+
+  const handleStateChange = (fieldName: keyof FormState, value: string) => {
+    setForm((prevForm) => ({ ...prevForm, [fieldName]: value }));
+  };
+
+  const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+
+    const file = e.target.files?.[0];
+
+    if (!file) return;
+
+    if (!file.type.includes("image")) {
+      alert("Please upload an image!");
+
+      return;
+    }
+
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+
+    reader.onload = () => {
+      const result = reader.result as string;
+
+      handleStateChange("image", result);
+    };
+  };
+
+  const handleFormSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    setSubmitting(true);
+
+    const { token } = await fetchToken();
+
+    try {
+      if (type === "create") {
+        await createNewProject(form, session?.user?.id, token);
+
+        router.push("/");
+      }
+
+      if (type === "edit") {
+        await updateProject(form, project?.id as string, token);
+
+        router.push("/");
+      }
+    } catch (error) {
+      alert(
+        `Failed to ${
+          type === "create" ? "create" : "edit"
+        } a project. Try again!`
+      );
+    } finally {
+      setSubmitting(false);
+    }
+  };
 
   return (
     <form onSubmit={handleFormSubmit} className="flexStart form">
@@ -93,14 +100,13 @@ const ProjectForm = ({ type, session, project }: Props) => {
           accept="image/*"
           required={type === "create" ? true : false}
           className="form_image-input"
-          onChange={handleChangeImage}
+          onChange={(e) => handleChangeImage(e)}
         />
-
         {form.image && (
           <Image
             src={form?.image}
             className="sm:p-10 object-contain z-20"
-            alt="project poster"
+            alt="image"
             fill
           />
         )}
@@ -109,27 +115,31 @@ const ProjectForm = ({ type, session, project }: Props) => {
       <FormField
         title="Title"
         state={form.title}
-        placeholder="Developedian"
+        placeholder="Flexibble"
         setState={(value) => handleStateChange("title", value)}
       />
+
       <FormField
         title="Description"
         state={form.description}
-        placeholder="Showcase and discover remarkable developer project."
+        placeholder="Showcase and discover remarkable developer projects."
+        isTextArea
         setState={(value) => handleStateChange("description", value)}
       />
+
       <FormField
         type="url"
         title="Website URL"
         state={form.liveSiteUrl}
-        placeholder="https://developedian.com"
+        placeholder="https://jsmastery.pro"
         setState={(value) => handleStateChange("liveSiteUrl", value)}
       />
+
       <FormField
         type="url"
-        title="Github URL"
+        title="GitHub URL"
         state={form.githubUrl}
-        placeholder="https://github.com/petherem"
+        placeholder="https://github.com/adrianhajdin"
         setState={(value) => handleStateChange("githubUrl", value)}
       />
 
@@ -143,13 +153,13 @@ const ProjectForm = ({ type, session, project }: Props) => {
       <div className="flexStart w-full">
         <Button
           title={
-            isSubmitting
+            submitting
               ? `${type === "create" ? "Creating" : "Editing"}`
               : `${type === "create" ? "Create" : "Edit"}`
           }
           type="submit"
-          leftIcon={isSubmitting ? "" : "/plus.svg"}
-          isSubmitting={isSubmitting}
+          leftIcon={submitting ? "" : "/plus.svg"}
+          isSubmitting={submitting}
         />
       </div>
     </form>
