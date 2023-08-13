d069946df024af9c309aedc3aa5838414879dc4d
Piotr Maciejewski <pether.maciejewski@gmail.com>
2023-08-13 18:49:38 +0200

    changes to home.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -25,9 +25,9 @@ type ProjectSearch = {
   },
 }
 
-// export const dynamic = 'force-dynamic';
-// export const dynamicParams = true;
-// export const revalidate = 0;
+export const dynamic = 'force-dynamic';
+export const dynamicParams = true;
+export const revalidate = 0;
 
 const Home = async ({ searchParams: { category, endcursor } }: Props) => {
   const data = await fetchAllProjects(category, endcursor) as ProjectSearch
@@ -37,7 +37,7 @@ const Home = async ({ searchParams: { category, endcursor } }: Props) => {
   if (projectsToDisplay.length === 0) {
     return (
       <section className="flexStart flex-col paddings">
-        {/* <Categories /> */}
+        <Categories />
 
         <p className="no-result-text text-center">No projects found, go create some first.</p>
       </section>
@@ -46,7 +46,7 @@ const Home = async ({ searchParams: { category, endcursor } }: Props) => {
 
   return (
     <section className="flexStart flex-col paddings mb-16">
-      {/* <Categories /> */}
+      <Categories />
 
       <section className="projects-grid">
         {projectsToDisplay.map(({ node }: { node: ProjectInterface }) => (
